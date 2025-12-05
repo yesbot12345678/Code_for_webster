5e1cdef1c028c1d141fa43bde54bccf8393cac41
yesbot12345678 <228827168+yesbot12345678@users.noreply.github.com>
2025-12-04 21:52:51 -0700

    dih

--- Chess.cpp
+++ Chess.cpp
@@ -6,13 +6,17 @@ using namespace std;
 
 void displayBoard(const vector<string>& chessBoard);
 void initalizeBoard(vector<string>& chessBoard);
-void player1Move();
-void player2Move();
+void player1Move(vector<string>& chessBoard);
+void player2Move(vector<string>& chessBoard);
 
 int main()
 {
     vector<string> chessBoard(8, string(8, '.'));
     initalizeBoard(chessBoard);
+    displayBoard(chessBoard);
+    player1Move(chessBoard);
+    displayBoard(chessBoard);
+    player2Move(chessBoard);
     displayBoard(chessBoard);
 
 
@@ -20,7 +24,7 @@ int main()
 }
 
 void displayBoard(const vector<string>& chessBoard) {
-    cout << "   1 2 3 4 5 6 7 8\n";
+    cout << "   a b c d e f g h\n";
     cout << " +-----------------+\n";
     for(int i = 0; i < 8; ++i) {
         cout << i + 1 << "| ";
@@ -37,18 +41,19 @@ void initalizeBoard(vector<string>& chessBoard) {
     chessBoard[6] = "PPPPPPPP";
     chessBoard[7] = "RNBQKBNR";
 }
-void player1Move() {
+void player1Move(vector<string>& chessBoard) {
     string move;
     cout << "Player 1, enter your move(format: e2 e4): "; 
     getline(cin, move);
     string letterFrom = move.substr(0,1);
     string letterTo = move.substr(3,1);
 
-    char numberFrom = move[1];
-    char numberTo = move[4];
-
-    int NumberFrom = numberFrom - 1;
-    int NumberFrom = numberTo - 1;
+    char FROM= move[1];
+    char TO= move[4];
+    int numberFrom = FROM - '0';
+    int numberTo = TO - '0';
+    numberFrom -= 1;
+    numberTo -= 1;
 
     int LetterFrom;
     int LetterTo;
@@ -105,7 +110,78 @@ void player1Move() {
         break;    
     }
 
+    chessBoard[numberTo][LetterTo] = chessBoard[numberFrom][LetterFrom];
+    chessBoard[numberFrom][LetterFrom] = '.';
 }
-void player2Move() {
-    
+void player2Move(vector<string>& chessBoard) {
+    string move;
+    cout << "Player 2, enter your move(format: e2 e4): "; 
+    getline(cin, move);
+    string letterFrom = move.substr(0,1);
+    string letterTo = move.substr(3,1);
+
+    char FROM= move[1];
+    char TO= move[4];
+    int numberFrom = FROM - '0';
+    int numberTo = TO - '0';
+    numberFrom -= 1;
+    numberTo -= 1;
+
+    int LetterFrom;
+    int LetterTo;
+    switch (letterFrom[0]) {
+        case 'a': 
+            LetterFrom = 0;
+        break;
+        case 'b':
+            LetterFrom = 1;
+        break;
+        case 'c':
+            LetterFrom = 2;
+        break;
+        case 'd':
+            LetterFrom = 3;
+        break;
+        case 'e':
+            LetterFrom = 4;
+        break;
+        case 'f':
+            LetterFrom = 5;
+        break;
+        case 'g':
+            LetterFrom = 6;
+        break;
+        case 'h':
+            LetterFrom = 7;
+        break;    
+    }
+    switch(letterTo[0]) {
+        case 'a': 
+            LetterTo = 0;
+        break;
+        case 'b':
+            LetterTo = 1;
+        break;
+        case 'c':
+            LetterTo = 2;
+        break;
+        case 'd':
+            LetterTo = 3;
+        break;
+        case 'e':
+            LetterTo = 4;
+        break;
+        case 'f':
+            LetterTo = 5;
+        break;
+        case 'g':
+            LetterTo = 6;
+        break;
+        case 'h':
+            LetterTo = 7;
+        break;    
+    }
+
+    chessBoard[numberTo][LetterTo] = chessBoard[numberFrom][LetterFrom];
+    chessBoard[numberFrom][LetterFrom] = '.';
 }
