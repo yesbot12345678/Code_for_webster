16fb9a4a4bd8a8c5faa297e387f75b794029f3b6
yesbot12345678 <kraftlouieschool@gmail.com>
2025-12-23 13:26:35 -0800

    dih

--- website_access.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = "https://buffstreams.plus/nflstreams2"
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
-
-soup.find_all('a')
