41fb5a4a35647466170fdf0eb0a4577a70227872
yesbot12345678 <kraftlouieschool@gmail.com>
2026-02-03 14:34:52 -0700

    done

---
+++ Programming II/python var.py
@@ -0,0 +1,14 @@
+Hello = 1
+Hello1 = "a"
+Hello2 = 1.5
+print("int:", Hello, "str:", Hello1, "float:", Hello2)
+print("i am using pascal case for my variable names.")
+print("Camel Case: you capitalize the first letter of each word except the first word.")
+print("Snake Case: you separate each word with an underscore and all letters are lowercase.")
+print("Pascal Case: you capitalize the first letter of each word including the first word.")
+Hello = input("Enter a number: ")
+print("You entered:", Hello)
+Hello = input("Enter a string: ")
+print("You entered:", Hello)
+Hello = input("Enter a float: ")
+print("You entered:", Hello)
