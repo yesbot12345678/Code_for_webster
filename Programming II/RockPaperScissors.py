d41698fca46ee24b9bca2ff09b8a19dfb943d604
Louie Kraft <Kraftlouieschool@gmail.com>
2026-02-19 14:36:13 -0700

    dih

--- Programming II/RockPaperScissors.py
+++ Programming II/RockPaperScissors.py
@@ -1,19 +1,19 @@
 import random
-l=0
+l=0 
 while l==0:
-    Com = random.randint(1,4)
+    Com = random.randint(1,4) #chooses a rand num from 1 to 3
     Player = str(input("enter rock, paper, or scissors (or leave to exit): "))
-    if Player == "leave" or Player == "Leave":
+    if Player == "leave" or Player == "Leave": # if player chose to exit
         print("exiting now")
-        l=1
-    elif Player == "rock" or Player == "Rock":
-        if Com == 1:
+        l=1 # setting the var to 1 so it fails the while loop check
+    elif Player == "rock" or Player == "Rock": 
+        if Com == 1: #if comp chooses rock
             print("tie game")
             print("Comp Chose Rock")
-        elif Com == 2:
+        elif Com == 2: #if comp chooses paper
             print("you lost")
             print("Comp Chose Paper")
-        elif Com == 3:
+        elif Com == 3: #if comp chooses scissors
             print("you win")
             print("Comp Chose Scissors")
     elif Player == "Paper" or Player == "paper":
