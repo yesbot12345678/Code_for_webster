78871188449e4222fcec0380e162058ce054580a
Louie Kraft <Kraftlouieschool@gmail.com>
2026-02-19 14:44:22 -0700

    done

--- Programming II/RockPaperScissors.py
+++ Programming II/RockPaperScissors.py
@@ -1,5 +1,8 @@
 import random
 l=0 
+p=0
+c=0
+t=0
 while l==0:
     Com = random.randint(1,4) #chooses a rand num from 1 to 3
     Player = str(input("enter rock, paper, or scissors (or leave to exit): "))
@@ -10,31 +13,67 @@ while l==0:
         if Com == 1: #if comp chooses rock
             print("tie game")
             print("Comp Chose Rock")
+            t += 1
+            print("comp has own", c, "times")
+            print("player has own", p, "times")
+            print("you have ties", t, "times")
         elif Com == 2: #if comp chooses paper
             print("you lost")
             print("Comp Chose Paper")
+            c += 1
+            print("comp has own", c, "times")
+            print("player has own", p, "times")
+            print("you have ties", t, "times")
         elif Com == 3: #if comp chooses scissors
             print("you win")
             print("Comp Chose Scissors")
+            p += 1
+            print("comp has own", c, "times")
+            print("player has own", p, "times")
+            print("you have ties", t, "times")
     elif Player == "Paper" or Player == "paper":
         if Com == 1:
             print("you win")
             print("Comp Chose Rock")
+            p += 1
+            print("comp has own", c, "times")
+            print("player has own", p, "times")
+            print("you have ties", t, "times")
         elif Com == 2:
             print("tie game")
             print("Comp Chose Paper")
+            t += 1
+            print("comp has own", c, "times")
+            print("player has own", p, "times")
+            print("you have ties", t, "times")
         elif Com == 3:
             print("you lost")
             print("Comp Chose Scissors")
+            c += 1
+            print("comp has own", c, "times")
+            print("player has own", p, "times")
+            print("you have ties", t, "times")
     elif Player == "scissors" or Player == "Scissors":
         if Com == 1:
             print("you lost")
             print("Comp Chose Rock")
+            c += 1
+            print("comp has own", c, "times")
+            print("player has own", p, "times")
+            print("you have ties", t, "times")
         elif Com == 2:
             print("you win")
             print("Comp Chose Paper")
+            p += 1
+            print("comp has own", c, "times")
+            print("player has own", p, "times")
+            print("you have ties", t, "times")
         elif Com == 3:
-            print("you lost")
+            print("tie game")
             print("Comp Chose Scissors")
+            t += 1
+            print("comp has own", c, "times")
+            print("player has own", p, "times")
+            print("you have ties", t, "times")
     else:
         print("acceptable value not entered try again")
