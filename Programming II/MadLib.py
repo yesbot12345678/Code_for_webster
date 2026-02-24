937ce76a1818b19ebef0f6fa9a43d80918683574
Louie Kraft <Kraftlouieschool@gmail.com>
2026-02-24 22:13:23 +0000

    done

---
+++ Programming II/MadLib.py
@@ -0,0 +1,11 @@
+for i in range(3):
+    if i == 0:
+        poem1 = "Mary had a little" + " " + input("Enter a noun: ") +" Its fleece was " + input("Enter an adjective: ") + " as snow." +" And everywhere that Mary went," +" the " + input("Enter another noun: ") + " was sure to go." +" It followed her to " + input("Enter another noun: ") + " one day," +" that was against the rules." +" It made the " + input("Enter another noun: ") + " laugh. and " + input("Enter a verb: ") + " to see the lamb at " + input("Enter another noun: ") + "."
+        print(poem1)
+    elif i == 1:
+        poem2 = "Nature's first " + input("Enter an adjective: ") + " is " + input("Enter a noun: ") +" Her hardest hue to hold." +" Her early leaf's a " + input("Enter another noun: ") +" But only so an hour."
+        print(poem2)
+    else:
+        poem3 = "A " + input("Enter a verb: ") + " little leaf of " + input("Enter an adjective: ") +" A bold audacious " + input("Enter another noun: ") +" A rendezvous, a " + input("Enter another verb: ") + " or two," +" And youth for ever lost."
+        print(poem3)
+    
