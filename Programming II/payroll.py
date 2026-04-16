008676c02232eec0f8a56bcbc606237d251a87e1
Louie Kraft <Kraftlouieschool@gmail.com>
2026-04-16 20:36:25 +0000

    dih

--- Programming II/payroll.py
+++ Programming II/payroll.py
@@ -1,19 +1,29 @@
 #base employee class
 class employee:
-    pay = 10
-    def __init__(self, first_name, last_name, emp_id):
+    pay_raise = 1.01
+    def __init__(self, first_name, last_name, emp_id, yearly_pay = 10):
         self.first_name = first_name
         self.last_name = last_name
         self.emp_id = emp_id
+        self.yearly_pay = yearly_pay
 #developer class
 class develper(employee):
-    pay = 100
+    pay_raise = 1.05
+    def __init__(self, first_name, last_name, emp_id, yearly_pay = 10):
+        super().__init__(first_name, last_name, emp_id, yearly_pay)
+        self.yearly_pay = yearly_pay * self.pay_raise
 #manager class
 class manager(employee):
-    pay = 1000
+    pay_raise = 1.08
+    def __init__(self, first_name, last_name, emp_id, yearly_pay = 10):
+        super().__init__(first_name, last_name, emp_id, yearly_pay)
+        self.yearly_pay = yearly_pay * self.pay_raise
 #creating employee, developer and manager objects
-epm1 = employee("John", "Doe", 123, )
-develper1 = develper("Jane", "Smith", 456)
-mannager1 = manager("Bob", "Johnson", 789)
 
+develper1 = develper("Jane", "Smith", 456)
+manager1 = manager("Bob", "Johnson", 789)
+
+
+print(develper1.pay_raise)
+print(manager1.pay_raise)
 print(help(develper))
