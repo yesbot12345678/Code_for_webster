291a03883fe457ec738282c0d7cd5491f778cb69
Louie Kraft <Kraftlouieschool@gmail.com>
2026-04-23 15:30:52 -0600

    dih

--- AP Comp. Sci./Balatro.cpp
+++ AP Comp. Sci./Balatro.cpp
@@ -1,11 +1,52 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-
-    string Jokers[] = {"joker", "greedy joker", "lusty Joker", "wrathful joker", "gluttonus joker", "jolly joker", 
+string Jokers[] = 
+{
+        "joker", "greedy joker", "lusty Joker", "wrathful joker", "gluttonus joker", "jolly joker", 
         "zany joker", "mad joker", "crazy joker", "droll joker", "sly joker", "wily joker", "clever joker", "devious joker", 
         "crafty joker", "half joker", "joker stencil", "four fingers", "mime", "credit card", "cerimonial dagger", "banner",
         "mystic summit", "marble joker", "loyalty card", "8 ball", "misprint", "dusk", "raised fist", "chaos the clown",
-        ""};
+        "fibonacci", "steel joker", "scary face", "abstract joker", "delayed gratification", "scholar", "business card",
+        "supernova", "ride the bus", "space joker", "hack","pareidolia", "gros michel", "even steven", "odd tom",
+        "egg", "burglar", "blackboard", "runner", "ice cream", "dna", "splash", "blue joker", "sixth sense", "constellation",
+        "hiker", "faceless joker", "green joker", "superpostion", "to do list", "cavendish", "card sharp", "red card",
+        "madness", "square joker", "seance", "riff-raff", "vampire", "shortcut", "hologram", "vagabond", "baron", "cloud 9",
+        "rocket", "obelisk", "midas mask", "luchador", "photograph", "gift card", "turtle bean", "erosion", "reserveed parking",
+        "mail-in rebate", "to the moon", "hallucination", "fortune", "juggler", "drunkard", "stone joker", "golden joker",
+        "lucky cat", "basebal card", "bull", "diet cola", "trading card", "flash card", "popcorn", "spare trousers", 
+        "ancient joker", "ramen", "walkie talkie", "seltzer", "castle", "smiley face", "campfire", "godlen ticket", "mr. bones",
+        "acrobat", "sock and buskin", "swasbuckler", "troubadour", "certificate", "smeared joker", "throwback", "hanging chad",
+        "rough gem", "bloodstone", "arrowhead", "onyx agate", "glass joker", "showman", "flower pot", "blueprint", "wee joker",
+        "merry andy", "oops! all 6s", "the idol", "seeing double", "matador", "hit the road", "the duo", "the trio", "the family",
+        "the order", "the tribe", "stuntman", "invisible joker", "brainstorm", "satellite", "shoot the moon", "driver's license",
+        "cartomancer", "astronomer", "brunt joker", "bootstraps", "canio", "triboulet", "yorick", "chicot", "perkeo" 
+};
+
+string decks[] = {};
+
+string vouchers[] = {};
+
+string blinds[] = {};
+
+string tags[] = {};
+
+string booster_packs[] = {};
+
+string tarrot[] = {};
+
+string planet[] = {};
+
+string spectral[] = {};
+
+string edditions[] = {};
+
+string seals[] = {};
+
+string enhancements[] = {};
+
+
+int main(){
+
+    
 }
