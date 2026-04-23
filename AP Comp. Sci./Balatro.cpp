b5950b4998ee7a44500190f815d531d86ba92636
yesbot12345678 <kraftlouieschool@gmail.com>
2026-04-23 14:38:30 -0600

    duh

---
+++ AP Comp. Sci./Balatro.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    string Jokers[] = {"joker", "greedy joker", "lusty Joker", "wrathful joker", "gluttonus joker", "jolly joker", 
+        "zany joker", "mad joker", "crazy joker", "droll joker", "sly joker", "wily joker", "clever joker", "devious joker", 
+        "crafty joker", "half joker", "joker stencil", "four fingers", "mime", "credit card", "cerimonial dagger", "banner",
+        "mystic summit", "marble joker", "loyalty card", "8 ball", "misprint", "dusk", "raised fist", "chaos the clown",
+        ""};
+}
