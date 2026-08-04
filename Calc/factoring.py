249054631c682d240ca4b005021abffa888b2fdb
Louie Kraft <Kraftlouieschool@gmail.com>
2026-08-04 16:01:03 +0000

    dih

---
+++ Calc/factoring.py
@@ -0,0 +1,46 @@
+print("Standard form for equation ax^2+bx+c ( ACCOUNT FOR THE SIGN )")
+
+ax=float(input("input a value for a: "))
+bx=float(input("input a value for b: "))
+c=float(input("input a value for c: "))
+
+x=int("x")
+
+z=0
+y=0
+
+while(False):
+    for n in range (a*c):
+        for m in range (a*c):
+            z=n*m
+            y=n+m
+            if z == a*c & y == b:
+                return True
+
+numberq1 = z
+factorsq1 = []
+
+for i in range(1, numberq1 + 1):
+    if numberq1 % i == 0:
+        factorsq1.append(i)\
+
+numberq2 = z
+factorsq2 = []
+
+for i in range(1, numberq2 + 1):
+    if numberq2 % i == 0:
+        factorsq2.append(i)\
+
+numberq3 = z
+factorsq3 = []
+
+for i in range(1, numberq3 + 1):
+    if numberq3 % i == 0:
+        factorsq3.append(i)\
+
+numberq1 = z
+factorsq1 = []
+
+for i in range(1, numberq1 + 1):
+    if numberq1 % i == 0:
+        factorsq1.append(i)\
